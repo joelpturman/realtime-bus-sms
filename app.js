1098e1becdd27d92d53796b1f668fba9d3f13f64
joelpturman <joelpturman@gmail.com>
2016-12-13 19:28:34 -0900

    Adding random variable
    tutorial for making changes via GitHub

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var rollbar = require("rollbar");
 var config = require('./lib/config');
+var myvariable = 'myvariable';
 
 rollbar.init(config.ROLLBAR_TOKEN);
 
